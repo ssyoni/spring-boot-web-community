9385e97d13a9fcff09ed71a84db27ccf391af01a
ssyoni <jsy950601@gmail.com>
2021-05-13 23:23:28 +0900

    kakao oauth2 mapping...

---
+++ src/main/java/com/web/oauth/ClientResources.java
@@ -0,0 +1,10 @@
+package com.web.oauth;
+
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+public class ClientResources {
+    
+    //  @NestedConfigurationProperty
+    //  private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+}
